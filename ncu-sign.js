794eed6bb7ef2602070326cb16a55a70eda47de0
John Wang <john29917958@gmail.com>
2016-12-23 19:25:18 +0800

    Fix casper exit issue

--- ncu-sign.js
+++ ncu-sign.js
@@ -10,7 +10,7 @@ var casper = require('casper').create({
         },
         viewportSize: { width: 1024, height: 768 }
     }),
-	app = require('./app.js'),
+    app = require('./app.js'),
     action = casper.cli.args[0],
     portalId = casper.cli.args[1],
     portalPassword = casper.cli.args[2],
@@ -21,39 +21,44 @@ function checkArgs() {
     if (action !== 'sign-in' && action !== 'sign-out') {
         console.log('Unsupported action "' + action + '".\nThe action should be "sign-in" or "sign-out".');
         console.log(helpMessage);
-        casper.exit();
+        return false;
     }
 
     if (portalId === undefined || portalId === null || portalId.length === 0) {
         console.log('Please specify your portal ID');
         console.log(helpMessage);
-        casper.exit();
+        return false;
     }
 
     if (portalPassword === undefined || portalPassword === null || portalPassword.length === 0) {
         console.log('Please specify your portal password');
         console.log(helpMessage);
-        casper.exit();
+        return false;
     }
 
     if (jobId === undefined || jobId === null || jobId.length === 0) {
-    	console.log('Please specify the job ID for signing in / out');
-    	console.log(helpMessage);
-    	casper.exit();
+        console.log('Please specify the job ID for signing in / out');
+        console.log(helpMessage);
+        return false;
     }
+
+    return true;
 }
 
-checkArgs();
-
-switch (action) {
-    case 'sign-in':
-        console.log('About to sign in into NCU Human System...');
-        app.signIn(portalId, portalPassword, jobId);
-        break;
-    case 'sign-out':
-        console.log('About to sign out from NCU Human System...');
-        app.signOut(portalId, portalPassword, jobId);
-        break;
-    default:
-        console.log('Unsupported action.\nThe action should be "sign-in" or "sign-out".');
+if (checkArgs()) {
+    switch (action) {
+        case 'sign-in':
+            console.log('About to sign in into NCU Human System...');
+            app.signIn(portalId, portalPassword, jobId);
+            break;
+        case 'sign-out':
+            console.log('About to sign out from NCU Human System...');
+            app.signOut(portalId, portalPassword, jobId);
+            break;
+        default:
+            console.log('Unsupported action.\nThe action should be "sign-in" or "sign-out".');
+    }
+}
+else {
+    casper.exit();
 }
